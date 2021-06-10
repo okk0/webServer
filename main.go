e989f63c8740fee998ef2e22524fea2722ac13c3
okk0 <alexanderhoyle123@gmail.com>
2021-06-10 05:43:57 -0400

    basic server

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
+// Server 1 is a minial echo server
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
-	println("hello!")
+	http.HandleFunc("/", handler) //each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler echoes the Path component of the requested URL
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
